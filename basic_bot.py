f1f98c00917c060939a9c3b15ef55cca57073cf5
Rohit Patnaik <quanticle@gmail.com>
2012-03-03 17:04:33 -0600

    Removed hardcoded channel and nickname

--- basic_bot.py
+++ basic_bot.py
@@ -4,27 +4,35 @@ from twisted .internet import reactor
 from twisted.internet.endpoints import TCP4ClientEndpoint
 
 class HelloBot(irc.IRCClient):
+    def __init__(self, factory):
+        self.nickname = factory.bot_nickname
+        self.channel = factory.bot_channel
+        
     def signedOn(self):
-        self.setNick("quanticle_bot")
-        self.join("#PyMNtos")
+        self.setNick(self.nickname)
+        self.join(self.channel)
         print "Bot online" #DEBUG    
 
     def privmsg(self, sender, recipient, msg):
         print "Message received: %s" % (msg,) #DEBUG
-        if recipient == "#PyMNtos" and msg.startswith("quanticle_bot"):
-            self.say("#PyMNtos", "%s: Hello there" % (self.parseNickname(sender,)))
+        if recipient == self.channel and msg.startswith(self.nickname):
+            self.say(self.channel, "%s: Hello there" % (self.parseNickname(sender,)))
 
     def parseNickname(self, sender):
         """ Parses the nickname from the nickname!hostmask string that the server sends as the sender"""
         return sender[:sender.find("!")]
     
 class BotFactory(Factory):
+    def __init__(self, nickname, channel):
+        self.bot_nickname = nickname
+        self.bot_channel = channel
+        
     def buildProtocol(self, addr):
-        return HelloBot()
+        return HelloBot(self)
 
 if __name__ == "__main__":
     endpoint = TCP4ClientEndpoint(reactor, "chat.freenode.net", 6667)
-    d = endpoint.connect(BotFactory())
+    d = endpoint.connect(BotFactory("quanticle_bot", "#PyMNtos"))
     reactor.run()
 
         
