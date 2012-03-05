99cb656e543992e4432e7f279c2d4ecf252117f3
Rohit Patnaik <quanticle@gmail.com>
2012-03-04 19:20:36 -0600

    Added time and hello commands

--- basic_bot.py
+++ basic_bot.py
@@ -2,25 +2,59 @@ from twisted.words.protocols import irc
 from twisted.internet.protocol import Factory
 from twisted .internet import reactor
 from twisted.internet.endpoints import TCP4ClientEndpoint
-
+from datetime import datetime
+import re
 class HelloBot(irc.IRCClient):
     def __init__(self, factory):
         self.nickname = factory.bot_nickname
         self.channel = factory.bot_channel
+        self.commands = ['hello', 'time']
         
     def signedOn(self):
         self.setNick(self.nickname)
         self.join(self.channel)
-        print "Bot online" #DEBUG    
+        print "Bot online with nickname: %s" % (self.nickname,) #DEBUG    
 
     def privmsg(self, sender, recipient, msg):
-        print "Message received: %s" % (msg,) #DEBUG
-        if recipient == self.channel and msg.startswith(self.nickname):
-            self.say(self.channel, "%s: Hello there" % (self.parseNickname(sender,)))
+        print "Sender: %s\nRecipient: %s\nMessage received: %s\n" % (sender, recipient, msg) #DEBUG
+        if recipient == self.nickname or (recipient == self.channel and msg.startswith(self.nickname)):
+            command = self.parseCommand(msg)
+            if command:
+                self.sendReply(sender, self.executeCommand(command))
+            else:
+                self.sendReply(sender, "Invalid command")
 
     def parseNickname(self, sender):
         """ Parses the nickname from the nickname!hostmask string that the server sends as the sender"""
         return sender[:sender.find("!")]
+
+    def parseCommand(self, command_str):
+        command_regexp = "%s.*\\s*(?P<command> .*)" % (self.nickname,)
+        match = re.match(command_regexp, command_str)
+        if match:
+            given_command = match.group("command").strip()
+            for accepted_command in self.commands:
+                if given_command == accepted_command:
+                    return accepted_command
+            return None
+        else: #We should never get here, because of the filtering done in the privmsg event handler
+            return None
+
+    def executeCommand(self, command):
+        command_method = getattr(self, command)
+        return command_method()
+
+    def sendReply(self, recipient, message):
+        self.msg(self.parseNickname(recipient), message)
+       
+    def hello(self):
+        """This is a basic command that just returns \"hello there\""""
+        return "Hello there."
+
+    def time(self):
+        return str(datetime.now())
+
+            
     
 class BotFactory(Factory):
     def __init__(self, nickname, channel):
